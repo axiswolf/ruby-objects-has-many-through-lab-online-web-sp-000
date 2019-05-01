0827c09eeb55bd40909b3970acabd4068178448f
Karen V <karenvo@usc.edu>
2019-05-01 08:02:02 +0000

    Automatically backed up by Learn

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,21 +1,21 @@
 class Genre
   @@all = []
-    attr_accessor :name
-    def initialize(name)
-      @name = name
-      @@all << self
+  attr_accessor :name
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+  def self.all
+    @@all
+  end
+  def songs
+    Songs.all.select do |song|
+      song.genre == self
     end
-    def self.all
-      @@all
+  end
+  def artists
+    songs.map do |song|
+      song.artist
     end
-    def songs
-      Songs.all.select do |song|
-        song.genre == self
-      end
-    end
-    def artists
-      songs.map do |song|
-        song.artist
-      end
-    end
+  end
 end
