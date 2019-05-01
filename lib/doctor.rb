6552f41ee95e52f551b387479f2666a0790fccda
Karen V <karenvo@usc.edu>
2019-05-01 08:11:31 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -8,6 +8,14 @@ class Doctor
   def new_appointment(date, patient)
     Appointment.new(date,patient,self)
   end
-
-
+  def appointments
+    Appointment.all.select do |appointment|
+      appointment.doctor == self
+    end
+  end
+  def patients
+    appointments.map do |appt|
+      appt.patient
+    end
+    
 end
