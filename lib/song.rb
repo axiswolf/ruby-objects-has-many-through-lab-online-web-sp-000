3b70f990e8bf5245a5c9366c70383dd07fb566de
Karen V <karenvo@usc.edu>
2019-05-01 07:44:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,9 +1,12 @@
 class Song
+  @@all = []
   attr_accessor :name, :genre, :artist
   def initialize(name, artist, genre)
     @name = name
     @genre = genre
     @artist = artist
   end
-
+  def self.all
+    @@all 
+  end
 end
